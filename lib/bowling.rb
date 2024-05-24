189b0bbbc06301ed1fdbc637c78364252ae36bc5
mikufu0418 <meijiushenjian@gmail.com>
2024-05-24 15:24:35 +0900

    3

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,2 +1,28 @@
-class bowling
-end
+# bowling.rb
+# bowling.rb
+
+# ボウリングのスコアを管理するクラス
+class Bowling
+    # インスタンスを生成する時に処理が実行される
+    def initialize
+      # スコアの合計
+      @total_score = 0
+    end
+  
+    # スコアの合計を返す
+    def total_score
+      @total_score
+    end
+  
+    # スコアを追加する
+    def add_score(pins)
+      @total_score += pins
+    end
+  end
+
+#   @game=Bowling.new
+#   20.times do
+#     @game.add_score(1)
+#   end
+#     puts@game.total_score==20
+  
